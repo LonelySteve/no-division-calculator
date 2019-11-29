8d59a5ff71fec7cae400ce6f2dc09d33537e688e
jloeve <jianglufull@foxmail.com>
2019-11-29 20:05:49 +0800

    完成主模块设计 - 更名为 ndc，就是 no-division-calculator 的缩写 - 添加交互式计算器功能 - 添加简单的命令行功能

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from .parser.parser import Parser
-
-
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
---
+++ src/ndc.py
@@ -0,0 +1,41 @@
+import sys
+
+from .parser.parser import Parser
+
+
+def handle_args(args):
+    if len(args) > 1:
+        if args[1] == '-c':
+            if len(args) > 2:
+                p = Parser(args[2])
+                print(p.parse().value)
+                return
+        elif args[1] == '-h' or args[1] == '--help':
+            print("-c <expression> 计算指定表达式，结果输出到标准输出流")
+            print("-h, -help    显示此帮助消息")
+            return
+        interactive()
+
+
+def interactive():
+    # 打印欢迎信息
+    print("Welcome to the no-division-calculator!")
+    while True:
+        try:
+            expression = input(">")
+            if not expression.strip():
+                continue  # 跳过空语句
+            p = Parser(expression)
+            print(p.parse().value)
+        except KeyboardInterrupt as ex:
+            exit(0)
+        except Exception as ex:
+            print(ex)
+
+
+def main(args):
+    handle_args(args)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
